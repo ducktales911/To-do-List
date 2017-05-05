0e3f009353e704051353402d718dbe9f2b62d039
Thomas Hamburger <thomas@esigaretshop.nl>
2017-05-05 14:51:34 +0200

    Initial Commit

---
+++ thomashamburger-pset4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thomashamburger-pset4
+//
+//  Created by Thomas Hamburger on 05-05-17.
+//  Copyright © 2017 Thomas Hamburger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
