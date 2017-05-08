d08971c0a793a61a1436f8a883c5039ff367485a
Thomas Hamburger <thomas@esigaretshop.nl>
2017-05-08 17:01:44 +0200

    Maakt nu alleen nog gebruik van array om todos in op te slaan. SQL delen uitgecomment. Volgende stap is de array met SQL implementeren.

---
+++ thomashamburger-pset4/DatabaseStuff.swift
@@ -0,0 +1,10 @@
+//
+//  DatabaseStuff.swift
+//  thomashamburger-pset4
+//
+//  Created by Thomas Hamburger on 08-05-17.
+//  Copyright © 2017 Thomas Hamburger. All rights reserved.
+//
+
+import Foundation
+
--- thomashamburger-pset4/ViewController.swift
+++ thomashamburger-pset4/ViewController.swift
@@ -12,15 +12,15 @@ import SQLite
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var createTodo: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     
     private var db: Connection?
-    
-    // TODO: CREËER TABLE VIEW
     
     // The table consist of two columns: id and name.
     let todos = Table("todos")
     let id = Expression<Int64>("id")
     let name = Expression<String>("name")
+    var todosArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,39 +33,70 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     // TODO: ZORGEN DAT INT TER GROTE VAN DE TODO LIST GERETURND WORDT (MISSCHIEN GEWOON "id"?)
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return todosArray.count
     }
     
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool
+    {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == .delete
+        {
+              todosArray[indexPath.row] = ""
+//            let normalInt = indexPath.row
+//            let sixfourInt = Int64(normalInt)
+//            let todo = todos.filter(id == sixfourInt)
+//            do {
+//                try db?.run(todo.delete())
+//            } catch {
+//                print("could not delete todo")
+//            }
+            tableView.reloadData()
+        }
+    }
+
     // TODO: INVULLEN MET DATA DIE UIT SEARCH GEHAALD WORDT
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TodoCell
+        let todoEntry = todosArray[indexPath.row]
+        cell.todoLabel.text = todoEntry
         return cell
-        
     }
 
-    // INSERT INTO "todos" ("name") VALUES ('Boodschappen')
+    // INSERT INTO "todos" ("name") VALUES
     @IBAction func storeInDatabase(_ sender: Any) {
-        let insert = todos.insert(name <- createTodo.text!)
-        
-        do {
-            let rowId = try db!.run(insert)
-            print(rowId)
-        } catch {
-            print("Error creating todo: \(error)")
+        if let text = createTodo.text, !text.isEmpty {
+            // let insert = todos.insert(name <- text)
+            //do {
+                // let rowId = try db!.run(insert)
+                // print(rowId)
+                todosArray.append(text)
+                tableView.reloadData()
+           // } catch {
+            //    print("Error creating todo: \(error)")
+            //}
         }
     }
     
-    // TODO: ZOEKFUCNTIE: Wordt nu nog niet gebruikt
-    func searchInDatabase() {
+    // TODO: ZOEKFUCNTIE
+    func searchInDatabase(normalInt: Int) -> String {
+        let sixfourInt = Int64(normalInt)
+        var todoString = ""
         do {
-            for todo in try db!.prepare(todos.filter(name.like("boodschappen"))) {
+            for todo in try db!.prepare(todos.filter(id == sixfourInt)) {
             print("id: \(todo[id]), name: \(todo[name])")
+            todoString = todo[name]
             // TODO: Plaats data in table view cells
             }
         } catch {
             // Error handling.
             print("Cannot connect to database: \(error)")
+            todoString = "hai"
         }
+        return todoString
     }
     
     private func setupDatabase() {
@@ -79,9 +110,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             print("Cannot connect to database: \(error)")
         }
     }
-    
-    private func createTable() {
-        
+
+    private func createTable() {    
         do {
             try db!.run(todos.create(ifNotExists: true) { t in // CREATE TABLE "todos"
                 
